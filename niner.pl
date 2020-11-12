d2d5694c25120ba2c4574967dd2386f973bd5cce
George <geo.rog09@gmail.com>
2020-11-12 14:16:28 +0000

    Question 1.1

---
+++ niner.pl
@@ -0,0 +1,9 @@
+% -- Question 1.1 -- %
+animal( X )
+	:- member( X, [aardvark, kangaroo, antelope, coyote, dingo, donkey, elephant, horse, jaguar] ).
+
+vegetable( X )
+	:- member( X, [artichoke, cabbage, carrot, celery, leek, lettuce, marrow, onion, potato] ).
+
+mineral( X )
+	:- member( X, [anatase, basalt, cobolt, copper, galena, nickel, sodium, silver, zircon] ).
