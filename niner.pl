7a4bb18154dcb67a58a8a7c71609d70206675461
George <geo.rog09@gmail.com>
2020-11-12 15:17:22 +0000

    Start Question 1.3

--- niner.pl
+++ niner.pl
@@ -11,3 +11,16 @@ mineral( X )
 % -- Question 1.2 -- %
 spell( W, X )
 	:- atom_chars( W, X ).
+
+% -- Question 1.3 -- %
+/*alphabet( X )
+	:- X = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z].
+
+solution( X, R )
+	:- spell( X, W ),
+       nth0( 4, W, R1 ), nth0( 5, W, R2 ), nth0( 2, W, R3 ), nth0( 5, W, R4 ), nth0( 8, W, R5 ), nth0( 9, W, R6 ),
+       spell( R, [R1, R2, R3, R4, R5, R6] ).
+
+main
+	:- S = hey,
+       write( S ).*/
