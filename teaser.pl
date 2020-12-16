e4c066577daed700aae9e42c023ffe9ea470b2ed
George <geo.rog09@gmail.com>
2020-12-16 17:28:10 +0000

    Question 2.3

--- teaser.pl
+++ teaser.pl
@@ -4,11 +4,31 @@ par( N )
        number_chars( N, X ),
        length( X, 4 ),
        \+ member( '0', X ),
+       unique( X ).
     
-unique(
+unique( [] ).
+unique( [H|T] )
+    :- \+ member( H, T ),
+       unique( T ).
 
 % -- Question 2.2 -- %
 pars
     :- par( N ),
        write( N ),
        fail.
+
+% -- Question 2.3 -- %
+party( X, Y )
+    :- par( X ),
+       par( Y ),
+       number_chars( X, X1 ),
+       number_chars( Y, Y1 ),
+       append( X1, Y1, R ),
+       unique( R ),
+       missing_digit( R, D ),
+       (X mod D) =:= 0,
+       (Y mod D) =:= 0.
+
+missing_digit( L, R )
+    :- N = [1,2,3,4,5,6,7,8,9],
+       subtract( N, L, [R|_] ).
